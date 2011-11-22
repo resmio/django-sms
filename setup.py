e5c478cceef60f0d9057a3f72e9e914292c642dd
Niels Sandholt Busch <niels.busch@gmail.com>
2011-11-22 14:26:02 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='django-sms',
+    version='0.0.1-alpha',
+    description='An interface for django sms backends',
+    author='Niels Sandholt Busch',
+    author_email='niels.busch@gmail.com',
+    url='https://bitbucket.org/resmio/django-sms/',
+    long_description=open('README', 'r').read(),
+    packages=[
+        'sms',
+        'sms.backends',
+    ],
+    requires=[
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
